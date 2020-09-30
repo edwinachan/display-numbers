32083d5e08f62aaae3acee114f6309c4fb092d6b
Edwina Chan <edwina.chan@bbc.co.uk>
2020-09-30 18:23:39 +0100

    Create correctForSingleDigits

--- displaytime.js
+++ displaytime.js
@@ -1,18 +1,23 @@
 const { displayNumber: displayNumber } = require('./displaynumber')
 
+const correctForSingleDigits = arr => {
+    arr.forEach((time, index) => {
+        if (time.length < 2) {
+            arr[index] = '0'.concat(time);
+        }
+    })
+    return arr
+};
+
 exports.displayTime = () => {
     const hour = new Date().getHours().toString();
     const minute = new Date().getMinutes().toString();
     const seconds = new Date().getSeconds().toString();
     let timeArr = [hour, minute, seconds];
     
-    timeArr.forEach((time, index) => {
-        if (time.length < 2) {
-            timeArr[index] = '0'.concat(time);
-        }
-    })
+    let time = correctForSingleDigits(timeArr);
 
-    let result = timeArr.join(':')
+    let result = time.join(':')
 
     return displayNumber(result, 1, 1)
 }
